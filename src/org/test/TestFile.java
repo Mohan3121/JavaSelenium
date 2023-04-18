c7df0caeca3d541432c485f6b5abfa35c3bcd9e0
Mohan3121 <kingmohan2125@gmail.com>
2023-04-18 13:26:22 +0530

    added

---
+++ src/org/test/TestFile.java
@@ -0,0 +1,23 @@
+package org.test;
+
+public class TestFile {
+	
+	private void employeeId() {
+		System.out.println("Employee Id = 21");
+	}
+	
+	private void employeeName() {
+		System.out.println("Employee Name = Mohan");
+	}
+	public static void main(String[] args) {
+		TestFile tf = new TestFile();
+		tf.employeeId();
+		tf.employeeName();
+	}
+	
+	
+	
+	
+	
+
+}
